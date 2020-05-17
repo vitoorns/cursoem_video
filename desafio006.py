4e2f4e7c80909a3b0c5a848291e0bc4acf6dd0a5
Vitor Nascimento <vitoorns@gmail.com>
2020-05-17 20:44:53 -0300

    att do codigo desafio 06

--- desafio006.py
+++ desafio006.py
@@ -2,9 +2,7 @@
 #raiz quadrada ex: 5/(1/2) ***verificar isso
 
 n1 = int(input('Digite um numero: '))
-dobro = n1*2
-triplo = n1*3
 raiz = int(n1**(1/2))
 
-print('O número digitado é {}, o dobro dele é {}, o triplo é {} e a raiz dele é {}'.format(n1,dobro,triplo,raiz))
+print('O número digitado é {}, o dobro dele é {}, o triplo é {} e a raiz dele é {}'.format(n1, n1*2, n1*3, raiz))
 
